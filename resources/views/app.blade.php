34f9d2115e2af3b9ec863b84c426ea3a73b4cb99
Dikhsan Dwirangga Tibong <120387198+Dikhsantibong@users.noreply.github.com>
2026-06-30 00:08:50 +0800

    tambah txt

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="1uWn_6o5MvgSf1QokjUu-3aA50jRAYNNWmRlaPiRsSc" />
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
