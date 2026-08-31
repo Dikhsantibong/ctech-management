b43c9a3e45c77923301662882dbeb3f73f4ebaa0
Dikhsan Dwirangga Tibong <120387198+Dikhsantibong@users.noreply.github.com>
2026-09-01 01:15:05 +0800

    perbaikan

--- resources/js/components/letter-editor.tsx
+++ resources/js/components/letter-editor.tsx
@@ -1,24 +1,4 @@
-import ReactQuill from 'react-quill-new';
-import 'react-quill-new/dist/quill.snow.css';
-
-const modules = {
-    toolbar: [
-        [{ font: [] }, { size: ['small', false, 'large', 'huge'] }],
-        ['bold', 'italic', 'underline', 'strike'],
-        [{ color: [] }, { background: [] }],
-        [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        [{ indent: '-1' }, { indent: '+1' }],
-        ['clean'],
-    ],
-};
-
-const formats = [
-    'font', 'size',
-    'bold', 'italic', 'underline', 'strike',
-    'color', 'background',
-    'align', 'list', 'indent',
-];
+import LetterEditorTiptap from './letter-editor-tiptap';
 
 export default function LetterEditor({
     value,
@@ -30,15 +10,10 @@ export default function LetterEditor({
     className?: string;
 }) {
     return (
-        <div className={className}>
-            <ReactQuill
-                theme="snow"
-                value={value}
-                onChange={onChange}
-                modules={modules}
-                formats={formats}
-                className="bg-background [&_.ql-container]:min-h-[280px] [&_.ql-container]:font-serif [&_.ql-container]:text-sm [&_.ql-editor]:min-h-[280px]"
-            />
-        </div>
+        <LetterEditorTiptap
+            value={value}
+            onChange={onChange}
+            className={className}
+        />
     );
 }
